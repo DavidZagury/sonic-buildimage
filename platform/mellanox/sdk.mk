71657f43843f8a1611dc336cbfb468c3b2588a00
david.zagury <davidza@nvidia.com>
2022-10-03 17:24:33 +0300

    Update SDK

--- platform/mellanox/sdk.mk
+++ platform/mellanox/sdk.mk
@@ -16,13 +16,13 @@
 #
 MLNX_SDK_BASE_PATH = $(PLATFORM_PATH)/sdk-src/sx-kernel/Switch-SDK-drivers/bin/
 MLNX_SDK_PKG_BASE_PATH = $(MLNX_SDK_BASE_PATH)/$(BLDENV)/$(CONFIGURED_ARCH)/
-MLNX_SDK_VERSION = 4.5.2320
+MLNX_SDK_VERSION = 4.5.3900-146
 MLNX_SDK_ISSU_VERSION = 101
 
 MLNX_SDK_DEB_VERSION = $(subst -,.,$(subst _,.,$(MLNX_SDK_VERSION)))
 
 # Place here URL where SDK sources exist
-MLNX_SDK_SOURCE_BASE_URL = http://fit69.mtl.labs.mlnx/auto/sw_system_release/sx_sdk_eth/sx_sdk_eth-4.5.3106/SOURCES/
+MLNX_SDK_SOURCE_BASE_URL = http://fit69.mtl.labs.mlnx/auto/sw_system_release/sx_sdk_eth/sx_sdk_eth-4.5.3900-146/SOURCES/
 
 ifneq ($(MLNX_SDK_SOURCE_BASE_URL), )
 SDK_FROM_SRC = y
